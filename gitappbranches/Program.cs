fb735c33c0e8b0ad61c10d4ef019a2b09767aa57
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2019-03-09 18:19:39 +0200

    first commit

---
+++ gitappbranches/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitappbranches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hi");
+            Console.ReadLine();
+        }
+    }
+}
