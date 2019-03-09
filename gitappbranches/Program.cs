db8071330ebd39f38d201be1414e32a18f62f6a0
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2019-03-09 18:19:58 +0200

    fitst commit

--- gitappbranches/Program.cs
+++ gitappbranches/Program.cs
@@ -11,7 +11,7 @@ namespace gitappbranches
         static void Main(string[] args)
         {
 
-            Console.WriteLine("hi");
+            Console.WriteLine("hi hi");
             Console.ReadLine();
         }
     }
