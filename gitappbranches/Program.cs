971af255b68ccd614a0fb82cbc0bfc516b10f701
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2021-10-11 01:09:46 -0700

    changed "Hi" message

--- gitappbranches/Program.cs
+++ gitappbranches/Program.cs
@@ -11,7 +11,7 @@ namespace gitappbranches
         static void Main(string[] args)
         {
 
-            Console.WriteLine("hi hi");
+            Console.WriteLine("hi Anton");
             Console.ReadLine();
         }
     }
